cf1614e283c14a033da479c9622b467345fee284
Zhong Zhenyu <nczzy1997@gmail.com>
2018-11-13 22:52:33 +0800

    完成多线程编程

--- http-server.py
+++ http-server.py
@@ -1,26 +1,42 @@
 from socket import *
 import sys
+import threading
 
-server_socket = socket(AF_INET, SOCK_STREAM)
-host = 'localhost'
-server_port = 8080
-server_socket.bind((host, server_port))
-server_socket.listen(5)
 
-print('Starting up http-server, serving ./')
-print('Available on:')
-print('  http://' + host + ':' + str(server_port))
-print('Hit CTRL-BREAK to stop the server')
+def main():
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    host = 'localhost'
+    server_port = 8080
+    server_socket.bind((host, server_port))
+    server_socket.listen(5)
+    counter = 0
 
-while True:
-    connection_socket, address = server_socket.accept()
-    print()
-    print('Connected by: ', address[0] + ':' + str(address[1]))
+    print('Starting up http-server, serving ./')
+    print('Available on:')
+    print('  http://' + host + ':' + str(server_port))
+    print('Hit CTRL-BREAK to stop the server')
+
+    while True:
+        connection_socket, address = server_socket.accept()
+        counter += 1
+        thread = threading.Thread(
+            target=new_service,
+            args=(connection_socket, address, counter)
+        )
+        thread.start()
+
+    server_socket.close()
+    sys.exit()
+
+
+def new_service(connection_socket, address, counter):
+    print('[#' + str(counter) + '] Connected by: ', address[0] + ':' + str(address[1]))
 
     try:
-        message = connection_socket.recv(1024)
-        filename = message.split()[1]
-        print('Filename to get: ', filename[1:].decode())
+        connection_socket.settimeout(600)
+        message = connection_socket.recv(1024).split()
+        filename = message[1]
+        print('[#' + str(counter) + '] Request: ', message[0].decode() + ' ' + message[1].decode())
         f = open(filename[1:], 'rb')
         output_data = f.read()
         connection_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
@@ -31,9 +47,12 @@ while True:
         connection_socket.send('404 Not Found'.encode())
         connection_socket.send('\r\n'.encode())
     except IndexError:
-        print('IndexError!')
+        print('[#' + str(counter) + '] IndexError!')
+    except timeout:
+        print('[#' + str(counter) + '] Timeout!')
     finally:
         connection_socket.close()
 
-server_socket.close()
-sys.exit()
+
+if __name__ == '__main__':
+    main()
