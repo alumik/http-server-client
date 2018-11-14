88a5c1c809d9b6f77ace47b452a819de501d71a6
Zhong Zhenyu <nczzy1997@gmail.com>
2018-11-14 16:37:47 +0800

    进行代码重构

--- http-client.py
+++ http-client.py
@@ -3,20 +3,18 @@ import sys
 
 
 def main():
-    client_socket = socket(AF_INET, SOCK_STREAM)
-    host = sys.argv[1]
-    server_port = int(sys.argv[2])
-    filename = sys.argv[3]
-    client_socket.connect((host, server_port))
-    client_socket.send(('GET /' + filename + ' HTTP/1.1\r\n\r\n').encode())
+    cl_socket = socket(AF_INET, SOCK_STREAM)
+    host, srv_port, filename = sys.argv[1], int(sys.argv[2]), sys.argv[3]
+    cl_socket.connect((host, srv_port))
+    cl_socket.send(('GET /' + filename + ' HTTP/1.1\r\n\r\n').encode())
 
     while True:
-        data = client_socket.recv(2048)
+        data = cl_socket.recv(2048)
         if not data:
             break
         print(data.decode())
 
-    client_socket.close()
+    cl_socket.close()
 
 
 if __name__ == '__main__':
--- http-server.py
+++ http-server.py
@@ -4,71 +4,66 @@ import threading
 
 
 def main():
-    host = 'localhost'
-    server_port = 8080
-    www_root = 'www'
-
-    server_socket = socket(AF_INET, SOCK_STREAM)    
-    server_socket.bind((host, server_port))
-    server_socket.listen(5)
-    counter = 0
+    host, srv_port, www_root = get_config()
+    srv_socket = socket(AF_INET, SOCK_STREAM)
+    srv_socket.bind((host, srv_port))
+    srv_socket.listen(5)
+    con_number = 0
 
     print('Starting up http-server, serving ./' + www_root + '/')
     print('Available on:')
-    print('  http://' + host + ':' + str(server_port))
+    print('  http://' + host + ':' + str(srv_port))
     print('Hit CTRL-BREAK to stop the server')
 
     while True:
-        connection_socket, address = server_socket.accept()
-        counter += 1
+        con_socket, address = srv_socket.accept()
+        con_number += 1
         thread = threading.Thread(
-            target=new_service,
-            args=(connection_socket, address, counter, www_root)
+            target=new_connection,
+            args=(con_socket, address, con_number, www_root)
         )
         thread.start()
 
-    server_socket.close()
+    srv_socket.close()
     sys.exit()
 
 
-def new_service(connection_socket, address, counter, www_root):
+def get_config():
+    args = sys.argv[1:]
+    if not args:
+        return 'localhost', 8080, 'www'
+    return args[0], int(args[1]), args[2]
+
+
+def new_connection(con_socket, address, con_number, www_root):
     print(
-        '[#' +
-        str(counter) +
-        '] Connected by: ' +
-        address[0] +
-        ':' +
-        str(address[1])
+        '[#' + str(con_number) + '] ' +
+        'Connected by: ' + address[0] + ':' + str(address[1])
     )
+
     try:
-        connection_socket.settimeout(600)
-        message = connection_socket.recv(1024).split()
-        filename = message[1]
+        con_socket.settimeout(600)
+        message = con_socket.recv(2048).decode().split()
         print(
-            '[#' +
-            str(counter) +
-            '] Request: ' +
-            message[0].decode() +
-            ' ' +
-            message[1].decode()
+            '[#' + str(con_number) + '] ' +
+            'Request: ' + message[0] + ' ' + message[1]
         )
-        f = open(www_root + '/' + filename[1:].decode(), 'rb')
-        output_data = f.read()
-        connection_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-        connection_socket.send(output_data)
-        connection_socket.send('\r\n'.encode())
+        output_data = open(www_root + '/' + message[1][1:], 'rb').read()
+
+        con_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+        con_socket.send(output_data)
+        con_socket.send('\r\n'.encode())
     except ConnectionResetError:
-        print('[#' + str(counter) + '] ConnectionResetError!')
+        print('[#' + str(con_number) + '] ConnectionResetError!')
     except IOError:
-        connection_socket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
-        connection_socket.send('404 Not Found'.encode())
-        connection_socket.send('\r\n'.encode())
+        con_socket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
+        con_socket.send('404 Not Found\r\n'.encode())
     except IndexError:
-        print('[#' + str(counter) + '] IndexError!')
+        print('[#' + str(con_number) + '] IndexError!')
     except timeout:
-        print('[#' + str(counter) + '] Timeout!')
+        print('[#' + str(con_number) + '] Timeout!')
     finally:
-        connection_socket.close()
+        con_socket.close()
 
 
 if __name__ == '__main__':
